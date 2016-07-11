b7fe17bab9508cad204cf378909d98c21a150f93
ryancheunggit <ryanzjlib@gmail.com>
2016-07-11 14:42:07 -0400

    a simple demo for google search

--- GoogleSearch/GoogleSearch.py
+++ GoogleSearch/GoogleSearch.py
@@ -2,26 +2,56 @@ import requests
 from bs4 import BeautifulSoup
 
 def google_search(query_term):
+	'''
+		perform google search on the given search term
+		return 
+			1 dictionary: with sample text - url mappings
+			2 boolean: indicate whether there are reuslts or not
+			3 string: the number of results found by google
+	'''
 	html = ''
+	text_url_dict = dict()
+	results_count = ''
+	success = False
 	try:
+		# get the html from search page
 		html = requests.get('https://www.google.com/search?q=' + query_term).text
 	except:
 		print("can not access google")
 	if html is not '':
-		links = []
+		# parse the html page
 		soup = BeautifulSoup(html, 'html.parser')
+		# find the number of results returned from google
 		results = soup.find("div", {"id": "resultStats"})
+		results_count = results.getText().split(" ")[1]
 		print "There are " + results.getText() + " returned from google"
 		print "here are some examples: "
+		# parse the links
 		for link in soup.find_all('a'):
 			url = str(link.get('href'))
 			if '/url?' in url:
 				text = link.getText()
 				if str(text) != '' and 'Cached' not in text :
 					print link.getText()
-					links.append(url)
-		links = set(links)
+					text_url_dict[text] = url
+	
+	success = len(text_url_dict) > 0
+
+	return text_url_dict, success, results_count
 
 if __name__ == '__main__':
 	print 'example search for term "obama"'
 	google_search("obama")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GoogleSearch/README.md
@@ -0,0 +1,9 @@
+# Google Search
+
+Install requirements locally
+
+  ```
+  pip install -t . -r requirements.txt
+  ```
+
+Create a zip file of the project directory and upload to your AWS Lambda function.
